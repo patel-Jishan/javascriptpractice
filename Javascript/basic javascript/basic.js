d08cb6bb1cd7b6afa51098c0652738c647902a8b
jishan <jishanpatel.15072003@gmail.com>
2025-04-18 17:30:42 +0530

    first commit

---
+++ Javascript/basic javascript/basic.js
@@ -0,0 +1,21 @@
+var x=document.getElementById("id1");
+x.style.color="red";
+x.style.backgroundColor="yellow";
+x.style.marginLeft="550px";
+var y=document.getElementById("id2");
+y.style.width="100px";
+y.style.marginLeft="700px";
+
+
+var z=document.getElementById("id3");
+z.style.color="blue";
+z.style.backgroundColor="grey";
+z.style.fontSize="100px";
+
+var a=document.getElementsByClassName("cl1");
+a[0].style .color="red";
+a[0].style.marginLeft="700px";
+a[0].style. fontSize="50px";
+a[0].style. backgroundColor="blue";
+
+
---
+++ Javascript/basic javascript/learn.js
@@ -0,0 +1,20 @@
+var x=document.getElementById("id1");
+x.style.color="red";
+x.style.marginLeft="800px";
+x.style.backgroundColor="grey";
+
+
+var y=document.getElementById("id2");
+y.style.Width="100px";
+y.style.marginLeft="800px";
+y.style.borderRadius="50px";
+
+var a={};
+
+a.name="Param";
+a.age=78;
+x.rollnumber=1244;
+console.log(a);
+
+
+
